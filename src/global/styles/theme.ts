bb400d4e7490eb10ef846d27bac5e4c91435ce54
xoblinha moises <xoblinha@MacBook-Pro-de-xoblinha.local>
2021-06-05 17:27:59 -0300

    Ädd colors theme

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -4,4 +4,15 @@ export default {
     medium: "Poppins_500Medium",
     bold: "Poppins_700Bold",
   },
+  colors: {
+    header: "#040404",
+
+    background: "#FDFDFD",
+    shape: "#F5F5F5",
+
+    pastas: "#7EB6FF",
+
+    text: "#040404",
+    text_dark: "#D6D6D6",
+  },
 };
