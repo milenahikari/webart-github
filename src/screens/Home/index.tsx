d9a0721893a811ce2f06504382b11d0a87479801
xoblinha moises <xoblinha@MacBook-Pro-de-xoblinha.local>
2021-06-05 18:24:19 -0300

    Add header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Header } from "../../components/Header";
 
 import * as S from "./styles";
 
 export function Home() {
   return (
     <S.Container>
+      <Header />
       <S.Texto>Teste fonte</S.Texto>
     </S.Container>
   );
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,8 +2,6 @@ import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
 `;
 
 export const Texto = styled.Text`
